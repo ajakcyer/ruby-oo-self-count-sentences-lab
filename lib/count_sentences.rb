9beb0b72fb94b080b992508fe2dde772ec4d2e5e
ajakcyer <ajakcyer97@gmail.com>
2020-09-17 22:13:35 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
 
+    # binding.pry
+    sentence_split = self.split(/[.!?]/).delete_if do |sentence|
+      sentence.size < 2
+    end
+    sentence_split.count
   end
 end
